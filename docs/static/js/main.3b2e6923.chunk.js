23d602053fc2e700e9971c0970ce963b378fc5bc
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2020-11-23 17:38:51 +0100

    Update run docs

---
+++ docs/static/js/main.3b2e6923.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmytemplete=this.webpackJsonpmytemplete||[]).push([[0],{10:function(e,t,n){},11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n(0),s=n.n(r),i=n(3),o=n.n(i);n(9),n(10);var a=function(){return Object(c.jsx)("div",{className:"App",children:"Hola"})};o.a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(a,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.3b2e6923.chunk.js.map
